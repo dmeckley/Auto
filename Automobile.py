43c95025a2de29187b15079053f6f3d01f9091b1
dmeckley <34632714+dmeckley@users.noreply.github.com>
2019-04-06 15:00:33 -0400

    Auto base class inheritance with python.
    Python 3.7 Inheritance with the Auto base class and three derived classes.

---
+++ Automobile.py
@@ -0,0 +1,155 @@
+# Dustin Meckley
+# Automobile.py
+# Base Class --> Auto
+# Derived Classes --> Car, Truck, SUV
+
+# BASE CLASS:
+class Auto:
+
+    # Constructor Method:
+    def __init__(self, year=None, make=None, model=None, engine=None, transmission=None, drivetrain=None):
+        # Default and Overloaded Constructor Method:
+
+        # Set Automobile Class Data Attributes:
+        self.__year = year
+        self.__make = make
+        self.__model = model
+        self.__engine = engine
+        self.__transmission = transmission
+        self.__drivetrain = drivetrain
+
+    # Mutator Methods:
+    def setYear(self, year):
+        self.__year = year
+
+    def setMake(self, make):
+        self.__make = make
+
+    def setModel(self, model):
+        self.__model = model
+
+    def setEngine(self, engine):
+        self.__engine = engine
+
+    def setTransmission(self, transmission):
+        self.__transmission = transmission
+
+    def setDriveTrain(self, drivetrain):
+        self.__drivetrain = drivetrain
+
+    # Accessor Methods:
+    def getYear(self):
+        return(self.__year)
+    
+    def getMake(self):
+        return(self.__make)
+
+    def getModel(self):
+        return(self.__model)
+
+    def getEngine(self):
+        return(self.__engine)
+
+    def getTransmission(self):
+        return(self.__transmission)
+
+    def getDriveTrain(self):
+        return(self.__drivetrain)
+
+    # Object Representation Method:
+    def __repr__(self):
+        return("{!r} {!r} {!r} {!r} {!r} {!r}".format(self.getYear(), self.getMake(), self.getModel(), self.getEngine(), self.getTransmission(), self.getDriveTrain()))
+
+    # Object String Method:
+    def __str__(self):
+        return("{0} {1} {2} {3} {4} {5}".format(str(self.getYear()), self.getMake(), self.getModel(), self.getEngine(), self.getTransmission(), self.getDriveTrain()))
+
+
+# DERIVED CLASS:
+class Car(Auto):
+
+    # Constructor Method:
+    def __init__(self, year=None, make=None, model=None, engine=None, transmission=None, drivetrain=None, door=None):
+        # Default and Overloaded Constructor Method:
+
+        # Call Base Class Method (Inheritance):
+        super().__init__(year, make, model, engine, transmission, drivetrain)
+
+        # Set Car Class Data Attributes:
+        self.__door = door
+
+    # Mutator Methods:
+    def setDoor(self, door):
+        self.__door = door
+
+    # Accessor Methods:
+    def getDoor(self):
+        return(self.__door)
+
+    # Object Representation Method:
+    def __repr__(self):
+        return(super().__repr__() + " {!r}dr".format(self.getDoor()))
+
+    # Object String Method:
+    def __str__(self):
+        return(super().__str__() + " {0}dr".format(str(self.getDoor())))
+
+
+# DERIVED CLASS:
+class Truck(Auto):
+
+    # Constructor Method:
+    def __init__(self, year=None, make=None, model=None, engine=None, transmission=None, drivetrain=None, bed=None):
+        # Default and Overloaded Constructor Method:
+
+        # Call Base Class Method (Inheritance):
+        super().__init__(year, make, model, engine, transmission, drivetrain)
+
+        # Set Truck Class Data Attributes:
+        self.__bed = bed
+
+    # Mutator Methods:
+    def setBed(self, bed):
+        self.__bed = bed
+
+    # Accessor Methods:
+    def getBed(self):
+        return(self.__bed)
+
+    # Object Representation Method:
+    def __repr__(self):
+        return(super().__repr__() + " {!r}bed".format(self.getBed()))
+
+    # Object String Method:
+    def __str__(self):
+        return(super().__str__ () + " {0}bed".format(self.getBed()))
+
+
+# DERIVED CLASS:
+class SUV(Auto):
+
+    # Constructor Method:
+    def __init__(self, year=None, make=None, model=None, engine=None, transmission=None, drivetrain=None, passenger=None):
+        # Default and Overloaded Constructor Method:
+
+        # Call Base Class Method (Inheritance):
+        super().__init__(year, make, model, engine, transmission, drivetrain)
+
+        # Set SUV Class Data Attributes:
+        self.__passenger = passenger
+
+    # Mutator Methods:
+    def setPassenger(self, passenger):
+        self.__passenger = passenger
+
+    # Accessor Methods:
+    def getPassenger(self):
+        return(self.__passenger)
+
+    # Object Representation Method:
+    def __repr__(self):
+        return(super().__repr__() + " {!r}pass".format(self.getPassenger()))
+
+    # Object String Method:
+    def __str__(self):
+        return(super().__str__() + " {0}pass".format(str(self.getPassenger())))
---
+++ main.py
@@ -0,0 +1,85 @@
+# Dustin Meckley
+# main.py
+# Base Class --> Auto
+# Derived Classes --> Car, Truck, SUV
+
+import Automobile
+
+
+def main():
+
+    # Creation of Auto object instance using constructor w/params:
+    print()
+    auto = Automobile.Auto(2005, 'Dodge', 'Dakota', 'V6', 'AT', '4WD')
+    autoRepr = repr(auto)
+    autoStr = str(auto)
+    print('auto -->', auto)
+    print('repr(auto) -->', autoRepr)
+    print('str(auto) -->', autoStr)
+
+    # Creation of Auto object instance using default constructor w/o params:
+    print()
+    newAuto = Automobile.Auto()
+    newAutoRepr = repr(newAuto)
+    newAutoStr = str(newAuto)
+    print('newAuto -->', newAuto)
+    print('repr(newAuto) -->', newAutoRepr)
+    print('str(newAuto) -->', newAutoStr)
+
+    # Building an Auto object instance from previous use of default constructor:
+    print()
+    newAuto.setYear(2019)
+    newAuto.setMake("Toyota")
+    newAuto.setModel("Camry")
+    newAuto.setEngine("Inline 4")
+    newAuto.setTransmission("AT")
+    newAuto.setDriveTrain("2WD")
+    newAutoRepr = repr(newAuto)
+    newAutoStr = str(newAuto)
+    print('newAuto -->', newAuto)
+    print('repr(newAuto) -->', newAutoRepr)
+    print('str(newAuto) -->', newAutoStr)
+
+    # Creation of a derived Car object instance inheriting from the Auto base class: 
+    print()
+    car = Automobile.Car()
+    car.setYear(1969)
+    car.setMake("Chevy")
+    car.setModel("Chevelle")
+    car.setEngine("V8")
+    car.setTransmission("AT")
+    car.setDriveTrain("2WD")
+    car.setDoor(2)
+    carRepr = repr(car)
+    carStr = str(car)
+    print('car -->', car)
+    print('repr(car) -->', carRepr)
+    print('str(car) -->', carStr)
+
+    print()
+    truck = Automobile.Truck()
+    truck.setYear(1957)
+    truck.setMake("Chevy")
+    truck.setModel("Cameo")
+    truck.setEngine("5.7")
+    truck.setTransmission("AT")
+    truck.setDriveTrain("2WD")
+    truck.setBed("Long")
+    truckRepr = repr(truck)
+    truckStr = str(truck)
+    print('truck -->', truck)
+    print('repr(truck) -->', truckRepr)
+    print('str(truck) -->', truckStr)
+
+    print()
+    suv = Automobile.SUV(2001, "Cadillac", "Escalade", "V8", "AT", "2WD", 8)
+    suvRepr = repr(suv)
+    suvStr = str(suv)
+    print('suv -->', suv)
+    print('repr(suv) -->', suvRepr)
+    print('str(suv) -->', suvStr)
+    print()
+
+
+if __name__ == "__main__":
+    main()
